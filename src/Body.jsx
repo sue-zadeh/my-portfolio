7eae7d7e95b3a887d0330396ce653fa47c0a9410
Sue Raisianzadeh <49556690+sue-raisianzadeh@users.noreply.github.com>
2023-05-24 22:37:46 +1200

    Update Body.jsx

--- src/Body.jsx
+++ src/Body.jsx
@@ -62,11 +62,10 @@ function Body() {
         </h1>
         <p class="p1">
           I am a dedicated and adaptable individual with a strong passion for acquiring new skills.
-          With a Software Development L6 certification from Dev Academy and a Technology Product 
-          Development certification from Otago Polytechnic, I have demonstrated my commitment to continuous 
-          learning and self-improvement. I thrive on challenges and have a positive outlook on my abilities.
-          Joining your team would be an excellent opportunity for me to learn and grow, 
-            and I am genuinely excited about the prospect of contributing to and learning from your team.
+          With a Software Development L6 certification from Dev Academy, I have demonstrated my
+          commitment to continuous learning and self-improvement. I thrive on challenges and have a 
+          positive outlook on my abilities. Joining your team would provide an excellent opportunity for me to learn,
+            contribute, and grow. I am genuinely excited about the prospect of being part of your team and making a positive impact.
         </p>
       </div>
 
