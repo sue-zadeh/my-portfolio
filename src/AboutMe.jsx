3070fe92089f3633509d647b31cf1ee9fe0c3ec6
Sue Raisianzadeh <raisianz@gmail.com>
2023-06-01 20:29:26 +1200

    about

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -11,32 +11,38 @@ function AboutMe() {
       </div>
       <div>
         <h1 className="h-me2">
-          <MdDeveloperBoard />
+          <MdDeveloperBoard
+            size={50}
+            color="#1a032f"
+            style={{ paddingRight: '10px', paddingLeft: '10px' }}
+          />
           About Me
         </h1>
         <p className="p-me2">
-          I was a teacher in my country when I discovered my passion for
-          technology. I needed to learn computer skills to become a registrar at
-          my school, and I found it fascinating. I learned MCSE and MCSA
-          certifications and even built a website for a real estate and
-          agricultural company. In addition, I pursued short online courses on
-          Azure, cloud computing, and AWS, earning certificates in these areas.
-          When I moved to New Zealand, I decided to enroll in Dev Academy to
-          further expand my knowledge. It was an intensive course, but I loved
-          every moment of it. At Dev Academy, I've learned different programming
-          languages like HTML5, CSS3, JavaScript, and Typescript. I also have
-          experience working with databases like SQLite3 and MySQL, and I
-          enjoyed working with ReactJS, P5, and Phaser3 during my time at Dev
-          Academy. In addition to technical skills, I have gained valuable human
-          skills, including teamwork, collaboration, and problem-solving through
-          debugging. I am driven by a passion for technology, and I hold
-          certificates in Azure, cloud computing, and AWS, along with my ISTQB
-          certification.
+          I was working as a teacher in my home country when I discovered my
+          passion for technology. As I aspired to become a registrar at my
+          school, I recognized the need to acquire computer skills, and I found
+          the subject fascinating. I pursued MCSE and MCSA certifications and
+          even developed a website for a real estate and agricultural company.
           <br />
-          I believe that my technical and human skills, coupled with my desire
-          to continuously improve, will enable me to succeed and make a positive
-          impact in the industry, and I hope to find a job in tech to continue
-          improving my skills and pursue my passion.
+          Upon moving to New Zealand, I made the decision to enroll in Dev
+          Academy to further expand my knowledge. It was an intensive course,
+          but I thoroughly enjoyed every moment of it. During my time at Dev
+          Academy, I gained proficiency in various programming languages such as
+          HTML5, CSS3, JavaScript, and Typescript. I also acquired experience
+          working with databases like SQLite3 and MySQL, and I had the
+          opportunity to work with ReactJS, P5, and Phaser3. Additionally, I
+          developed valuable interpersonal skills such as teamwork,
+          collaboration, and problem-solving through debugging.
+          <br />
+          Driven by my passion for technology, I pursued short online courses on
+          Azure, cloud computing, and AWS, earning certifications in these
+          areas, in addition to my ISTQB certification.
+          <br />I firmly believe that my technical expertise, coupled with my
+          interpersonal skills and desire for continuous improvement, will
+          enable me to succeed and make a positive impact in the tech industry.
+          I am eager to find a job in the field, where I can further enhance my
+          skills and pursue my passion.
           <br />
         </p>
       </div>
