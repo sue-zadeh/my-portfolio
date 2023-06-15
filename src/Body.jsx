e1abd49ee5b25696cb96f72f3606ff0508699a97
Sue Raisianzadeh <raisianz@gmail.com>
2023-06-15 18:05:59 +1200

    updating

--- src/Body.jsx
+++ src/Body.jsx
@@ -30,7 +30,7 @@ function Body() {
         {/* <p className="p4">Full Stack Web Developer.</p> */}
         <h4>Tech Skills:</h4>
         <p className="p4">
-          HTML5, CSS3, Tailwind, SCSS, Java Script, TypeScript, Reactjs,
+          HTML5, CSS3, Tailwind, SCSS, Javascript, Typescript, Reactjs,
           Debugging, Microsoft Cloud, Azure Fundamentals, AWS, ISTQB, P5,
           Phaser3, SQL, Squlit3, API, somnia, Postman, Netlify, Heroku, Github,
           Adobe Dream weaver, Adobe Photoshop, visual studio. The capacity for
