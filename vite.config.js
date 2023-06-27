282f15b5aa30a62037beced24e1f6608fa97bc4a
Sue Raisianzadeh <raisianz@gmail.com>
2023-06-27 15:29:56 +1200

    update target ip

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:${process.env.PORT || 3000}`,
+        target: `http://0.0.0.0:${process.env.PORT || 3000}`,
         changeOrigin: true,
       },
     },
