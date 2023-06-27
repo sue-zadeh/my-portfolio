de97e5fb2dd561a139a9d0cdd48caa99b7973f78
Sue Raisianzadeh <raisianz@gmail.com>
2023-06-27 16:08:27 +1200

    server index html from dist

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
-
+const { join } = require('path')
 const server = express()
 const port = process.env.PORT || 3000
 
 server.use(express.json())
+server.use(express.static(join(__dirname, '..', 'dist')))
 server.use('/api', routes)
+
+if (process.env.NODE_ENV === 'production') {
+  server.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'dist', 'index.html'))
+  })
+}
 
 server.listen(port, () => {
   console.log('Listening on port', port)
