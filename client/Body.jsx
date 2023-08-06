c38a2a370429ab3c2955effe5c42be40febcd108
Sue Raisianzadeh <raisianz@gmail.com>
2023-08-06 14:33:28 +1200

    new cv

--- client/Body.jsx
+++ client/Body.jsx
@@ -83,7 +83,7 @@ function Body() {
         <p className="pp4">
           Need a printable version of my CV? Download it here &nbsp;
           <a
-            href="https://drive.google.com/file/d/17-Q4UZy5bQx5rL0KB1rfy1mSRq7NJI_9/view?usp=sharing"
+            href="https://drive.google.com/file/d/17R7Z3qglfvFgV5sx51QYVAzmzSltkMBi/view?usp=sharing"
             target="_blank"
           >
             <ImDownload2 size={28} color="white" />
