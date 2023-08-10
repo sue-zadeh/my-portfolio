7ca08a64792988ba0673bcd56763ed5a08f533c0
Sue Raisianzadeh <raisianz@gmail.com>
2023-08-11 09:13:33 +1200

    projects

--- client/Projects.jsx
+++ client/Projects.jsx
@@ -21,57 +21,49 @@ function Projects() {
 
   return (
     <div className="projects">
-      {/* <div className="navpro">
+      <div className="navpro">
         <NavBar />
-      </div> */}
+      </div>
       <div className="projects1">
         <h2 className="h2-projects">Projects</h2>
-
-        <div className="wrap-projects">
-          <div className="div-squish">
-            <p className="p-projects">
-              <b>
-                This website ( DevStagram ) was a one-day group project that
-                retrieves information, displays it on another page, and allows
-                users to add data to the JSON using React.js, Redux.js and
-                Typescript. I would like to improve this project by turning it
-                into a chatroom with media. I worked on the software sign-in
-                page and served as the git leader.
-              </b>
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/DevStagram"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Devstagram} alt="Devstagram" />
-            </p>
-          </div>
-
-          <div className="div-squish">
-            <p className="p-projects">
-              Currently, I am working on improving the 'DevStagram' project by
-              building a chatroom with media capabilities. The changes have been
-              saved in the 'chatroom-app' repository.
-              <b>
-                I am using Firebase and Realtime Database to implement the
-                necessary functionalities.{' '}
-              </b>
-              <b> ( On going )</b>
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/chatroom-App"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Chat} alt="Chat" />{' '}
-            </p>
-          </div>
+        <div className="div-squish">
+          <p className="p-projects">
+            <b>
+              This website ( DevStagram ) was a one-day group project that
+              retrieves information, displays it on another page, and allows
+              users to add data to the JSON using React.js, Redux.js and
+              Typescript. I would like to improve this project by turning it
+              into a chatroom with media. I worked on the software sign-in page
+              and served as the git leader.
+            </b>
+            <br />
+            <b>
+              Currently, I am in the process of improving this project by
+              transforming it into a chatroom with media capabilities. The
+              changes have been saved in the chatroom-app repository. <br />
+              I used Firebase and Realtime Database to build the necessary
+              functionalities.
+              <br /> <b> ( On going )</b>
+            </b>
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/DevStagram"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/chatroom-App"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Chat} alt="Chat" />
+            <img className="picsquish" src={Devstagram} alt="Devstagram" />
+            {/* <br /> */}
+          </p>
         </div>
-        {/* <br /> */}
-
         {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
 
         {/* className="pro1"
@@ -79,137 +71,132 @@ function Projects() {
           onMouseLeave={() => setShowText(false)} */}
 
         {/* <div className="rows"> */}
-        <div className="wrap-projects">
-          {/* {showText && ( */}
-          {/* // <div className="text-layer"> */}
-          {/* show the text layer when showText is true */}
-          {/* </div>
-        </div> */}
-          {/* <div className="div-squish">
+        <div className="div-squish">
           <p className="p-projects">
-          The website provides list of movies based on genres, using React.js,
-          Typescript and API . It was a good practice to learn more about API
-          and backend
-          <br />
-          <a
-          className="aa"
-          href="https://github.com/sue-raisianzadeh/Flimflix"
+            <b>
+              It was a final group project, which lasted 1 week, and is a game
+              Using p5 library, Reactjs, Redux.js, jestT and ypescript . we
+              thought about using phaser3 instead of p5, but finally we decided
+              to use p5. But that was good as I learnt phaser3 too. I worked on
+              some part of frontend, backend, git and wrote tests for some
+              pages.
+            </b>
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/Squishy-Squish"
               target="blank"
-              >
+            >
               view my project here
-              </a>
-              <img className="picsquish" src={Devflix} alt="Flim" />
-              </p>
-            </div> */}
-          {/* </div> */}
-          <div className="div-squish">
-            <p className="p-projects">
-              This website was a one-day group project. It is a game created
-              using React.js, Redux.js, Typescript. In the group, my role was to
-              work as the frontend lead.
-              <br />
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/no-deal-or-deal"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Nodeal} alt="Nodeal" />
-            </p>
-          </div>
-          <div className="div-squish">
-            <p className="p-projects">
-              <b>
-                It was a final group project, which lasted 1 week, and is a game
-                Using p5 library, Reactjs, Redux.js, jestT and ypescript . we
-                thought about using phaser3 instead of p5, but finally we
-                decided to use p5. But that was good as I learnt phaser3 too. I
-                worked on some part of frontend, backend, git and wrote tests
-                for some pages.
-              </b>
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/Squishy-Squish"
-                target="blank"
-              >
-                built view my project here
-              </a>
-              <img className="picsquish" src={Squish} alt="Jumpy" />
-            </p>
-          </div>
-          <div className="div-squish">
-            <p className="p-projects">
-              This website was a one-day group project, and it offers
-              information about Indian, Italian, Asian, and Vegan restaurants,
-              using CSS3 Javascript, server-side handling. I would like to
-              improve this project by adding a 'Search' function.
-              <br />
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/dev-eats"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Eat} alt="Eat" />
-            </p>
-          </div>
+            </a>
+            <img className="picsquish" src={Squish} alt="Jumpy" />
+          </p>
         </div>
-        <div className="wrap-projects">
-          <div className="div-squish">
-            <p className="p-projects">
-              This is the TIc-Tac-Toe game, using Javascript and CSS3. I built
-              this to have more practice on javascript. I plan to add autoplay
-              as a second player
-              <br />
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/tic-tac-toe"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Toe} alt="Toe" />
-            </p>
-          </div>
-          {/* <div className="rows"> */}
-          <div className="div-squish">
-            <p className="p-projects">
-              <b>
-                I built this website in February 2021 based on my self-studying
-                and online courses. It is about an Iranian music band (folk
-                music), created using HTML5 and CSS3. I want to transition this
-                website from HTML to ReactJS, and add back-end functionality to
-                the site.
-              </b>
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/music-band"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Music} alt="Music" />
-            </p>
-          </div>
-          <div className="div-squish">
-            <p className="p-projects">
-              This is the Minesweeper game, using Javascript and CSS3. I built
-              this to have more practice on javascript
-              <br />
-              <a
-                className="aa"
-                href="https://github.com/sue-raisianzadeh/minesweeper"
-                target="blank"
-              >
-                view my project here
-              </a>
-              <img className="picsquish" src={Minesweeper} alt="Minesweeper" />
-            </p>
-          </div>
+        {/* {showText && ( */}
+        {/* // <div className="text-layer"> */}
+        {/* show the text layer when showText is true */}
+        {/* </div>
+        </div> */}
+        {/* <div className="div-squish">
+          <p className="p-projects">
+            The website provides list of movies based on genres, using React.js,
+            Typescript and API . It was a good practice to learn more about API
+            and backend
+            <br />
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/Flimflix"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Devflix} alt="Flim" />
+          </p>
+        </div> */}
+        {/* </div> */}
+        <div className="div-squish">
+          <p className="p-projects">
+            This is the Minesweeper game, using Javascript and CSS3. I built
+            this to have more practice on javascript
+            <br />
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/minesweeper"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Minesweeper} alt="Minesweeper" />
+          </p>
         </div>
-
+        <div className="div-squish">
+          <p className="p-projects">
+            This is the TIc-Tac-Toe game, using Javascript and CSS3. I built
+            this to have more practice on javascript. I plan to add autoplay as
+            a second player
+            <br />
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/tic-tac-toe"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Toe} alt="Toe" />
+          </p>
+        </div>
+        {/* <div className="rows"> */}
+        <div className="div-squish">
+          <p className="p-projects">
+            This website was a one-day group project. It is a game created using
+            React.js,Redux.js, Typescript. In the group, my role was to work as
+            the frontend lead.
+            <br />
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/no-deal-or-deal"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Nodeal} alt="Nodeal" />
+          </p>
+        </div>
+        <div className="div-squish">
+          <p className="p-projects">
+            This website was a one-day group project, and it offers information
+            about Indian, Italian, Asian, and Vegan restaurants, using CSS3
+            Javascript, server-side handling. I would like to improve this
+            project by adding a 'Search' function.
+            <br />
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/dev-eats"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Eat} alt="Eat" />
+          </p>
+        </div>
+        <div className="div-squish">
+          <p className="p-projects">
+            <b>
+              I built this website in February 2021 based on my self-studying
+              and online courses. It is about an Iranian music band (folk
+              music), created using HTML5 and CSS3. I want to transition this
+              website from HTML to ReactJS, and add back-end functionality to
+              the site.
+            </b>
+            <a
+              className="aa"
+              href="https://github.com/sue-raisianzadeh/music-band"
+              target="blank"
+            >
+              view my project here
+            </a>
+            <img className="picsquish" src={Music} alt="Music" />
+          </p>
+        </div>
         {/* </div> */}
       </div>
     </div>
