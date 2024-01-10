5fe7a891a87e4032223f1d76440fd21d1f4b0579
Sue Raisianzadeh <raisianz@gmail.com>
2024-01-10 22:37:33 +1300

    css

--- client/Banner.jsx
+++ client/Banner.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FaLaptopCode, FaGraduationCap, FaLightbulb } from 'react-icons/fa'
 import { HiOutlineHand } from 'react-icons/hi'
-// import { HiOutlineHandWave } from 'react-icons/hi'
-// import { AiFillInfoCircle } from 'react-icons/ai'
-// import { RiUserFill } from 'react-icons/ri'
-// import { AiOutlineCode } from 'react-icons/ai'
 
 function Banner() {
   const [sentenceIndex, setSentenceIndex] = useState(0)
