35fa5d41c982954971d382c9ba3b549597778657
Sue Raisianzadeh <raisianz@gmail.com>
2024-01-21 12:51:42 +1300

    body

--- client/Body.jsx
+++ client/Body.jsx
@@ -41,11 +41,7 @@ function Body() {
         <p className="pp4">
           Need a printable version of my CV ?
           <br />
-          <a
-            className="download2"
-            href="https://drive.google.com/file/d/1j9NM-miBMdj-1y1ChvDZ9WCqzfJj03Vs/view?usp=sharing"
-            target="_blank"
-          >
+          <a className="download2" href="" target="_blank">
             Download it here
             <ImDownload2
               size={32}
