3bd1c93b9faa186a34d3dcc4e4d37e15e94dc245
Sue Raisianzadeh <raisianz@gmail.com>
2024-02-13 13:12:27 +1300

    form updateing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { join } = require('path');
 const server = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
