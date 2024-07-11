9ad9c7f4051f019a0691d2a600dedbe5f61a96d3
Sue Zadeh <suezadeh.a@gmail.com>
2024-07-12 08:51:37 +1200

    editing

--- client/AboutMe.jsx
+++ client/AboutMe.jsx
@@ -20,28 +20,20 @@ function AboutMe() {
           About Me
         </h1>
         <p className="p-me2">
-          I started as a teacher back in my home country, where I found my love
-          for technology. This new interest led me to learn computer skills, and
-          soon, I was totally into it. I even created websites for two
-          companies, one in real estate and the other in agriculture, and helped
-          set up computer classes at a high school where I taught and supervised
-          for six years. When I moved to New Zealand, I joined the Dev Academy
-          to learn even more. It was a tough course with about 700-800 hours
-          coding, but I loved every bit of it, picking up new tech skills on the
-          way. I didn't just learn about technology; I also got better at
-          working with people, listening, solving problems, and working as a
-          team. I'm really good at learning things quickly, working hard, and
-          sticking to what I believe in. I'm also pretty good at thinking
-          logically and creatively, which helps me solve problems in smart ways.
-          Right now, I'm volunteering at Smart Panel Homes Ltd, working on their
-          website using tools like ReactJS, php, Mysql, CSS, and Bootstrap. At
-          the same time, I'm studying online for a Salesforce certification. I'm
-          really focused on this because I love to learn and grow. In 2024, I
-          plan to start an online Master's degree in Applied Computing at
-          Lincoln University. I'm always looking for ways to get better at what
-          I do. I'm a quick learner and always ready to take on new challenges,
-          and looking for opportunities in the tech field where I can use my
-          technical and people skills, and keep growing in my career.
+          I am a motivated Software Engineer with Azure certification and more
+          than a year of development experience, supported by six years managing
+          computer workshops. My skills are continuously sharpened through
+          rigorous training at Dev Academy, including courses in Azure AI
+          Fundamentals, AWS, and ongoing studies in Salesforce, C# and .NET. I
+          am known for my ability to quickly master new technologies and excel
+          in dynamic, fast-paced settings. I am deeply committed to personal and
+          professional growth, consistently seeking opportunities to enhance my
+          programming and problem-solving abilities. My keen attention to detail
+          and strong time management skills enable me to excel in software
+          engineering, where I thrive under pressure and am always ready to
+          tackle new challenges. I aim to leverage my comprehensive technical
+          skills to drive innovation and efficiency in the tech industry. ive
+          spirit to make a meaningful impact in the tech industry.
           <br /> <br />
         </p>
       </div>
