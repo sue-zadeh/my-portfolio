09604011b81b6b06197c9e579795e30194e73964
root <root@Sue>
2024-09-15 19:47:05 +1200

    fixing

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -6,25 +6,25 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3'),
+      filename: path.join(__dirname, 'dev.sqlite3')
     },
     useNullAsDefault: true,
     migrations: {
-      directory: path.join(__dirname, 'migrations'),
+      directory: path.join(__dirname, 'migrations')
     },
     seeds: {
-      directory: path.join(__dirname, 'seeds'),
-    },
+      directory: path.join(__dirname, 'seeds')
+    }
   },
 
   production: {
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: false }
     },
     migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+      tableName: 'knex_migrations'
+    }
+  }
 }
